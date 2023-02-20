32fa51f7e31c1d308efc38d070ec38aff3696745
Remir <tengubird800@gmail.com>
2023-02-20 15:42:47 +0300

    hmwrk done

---
+++ program2.java
@@ -0,0 +1,37 @@
+// Реализуйте очередь с помощью LinkedList со следующими методами:
+// enqueue() - помещает элемент в конец очереди,
+// dequeue() - возвращает первый элемент из очереди и удаляет его,
+// first() - возвращает первый элемент из очереди, не удаляя.
+
+import java.util.ArrayDeque;
+
+public class program2 {
+    public static ArrayDeque<Integer> enqueue(ArrayDeque<Integer> linkedList, int element) {
+        linkedList.addLast(element);
+        return linkedList;
+    }
+
+    public static int dequeue(ArrayDeque<Integer> linkedList) {
+        int firstElement = linkedList.getFirst();
+        linkedList.pollFirst();
+        System.out.println("Очередь после удаления первого элемента: " + linkedList);
+        return firstElement;
+    }
+
+    public static int first(ArrayDeque<Integer> linkedList) {
+        return linkedList.getFirst();
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> linkedList = new ArrayDeque<>();
+        for (int i = 0; i < 10; i++) {
+            linkedList.add((int) (Math.random() * (20 - 1)));
+        }
+        int element = 49;
+        System.out.println("Очередь: " + linkedList);
+        System.out.println("Очередь после добавления элемента: " + enqueue(linkedList, element) + "\n"
+                + "Добавляемый элемент: " + element);
+        System.out.println("Удаляемый элемент: " + dequeue(linkedList));
+        System.out.println("Первый элемент: " + first(linkedList));
+    }
+}
